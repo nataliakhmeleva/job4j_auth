d0118fc35613fd05fa5085daf192f2965a524780
Natalia Khmeleva <nataliya-mar@mail.ru>
2023-04-10 12:04:09 +0300

    5. Обработка исключений и Spring REST [#504797]

--- src/main/java/ru/job4j/auth/controller/PersonController.java
+++ src/main/java/ru/job4j/auth/controller/PersonController.java
@@ -1,19 +1,29 @@
 package ru.job4j.auth.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.auth.model.Person;
 import ru.job4j.auth.service.PersonService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
 @RequestMapping("/person")
 @AllArgsConstructor
 public class PersonController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PersonController.class.getSimpleName());
+    private final ObjectMapper objectMapper;
     private final PersonService persons;
     private BCryptPasswordEncoder encoder;
 
@@ -26,13 +36,20 @@ public class PersonController {
     public ResponseEntity<Person> findById(@PathVariable int id) {
         var person = this.persons.findById(id);
         return new ResponseEntity<>(
-                person.orElse(new Person()),
-                person.isPresent() ? HttpStatus.OK : HttpStatus.NOT_FOUND
+                person.orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Person is not found. Please, check login and password."
+                )), HttpStatus.OK
         );
     }
 
     @PostMapping("/")
     public ResponseEntity<Person> create(@RequestBody Person person) {
+        if (person.getLogin() == null || person.getPassword() == null) {
+            throw new NullPointerException("Login and password mustn't be empty");
+        }
+        if (person.getPassword().length() < 6) {
+            throw new IllegalArgumentException("Invalid password");
+        }
         return new ResponseEntity<>(
                 this.persons.save(person),
                 HttpStatus.CREATED
@@ -41,6 +58,12 @@ public class PersonController {
 
     @PutMapping("/")
     public ResponseEntity<Void> update(@RequestBody Person person) {
+        if (person.getLogin() == null || person.getPassword() == null) {
+            throw new NullPointerException("Login and password mustn't be empty");
+        }
+        if (person.getPassword().length() < 6) {
+            throw new IllegalArgumentException("Invalid password");
+        }
         return new ResponseEntity<>(this.persons.update(person)
                 ? HttpStatus.OK : HttpStatus.NOT_FOUND);
     }
@@ -56,7 +79,26 @@ public class PersonController {
 
     @PostMapping("/sign-up")
     public void signUp(@RequestBody Person person) {
+        if (person.getLogin() == null || person.getPassword() == null) {
+            throw new NullPointerException("Login and password mustn't be empty");
+        }
+        if (person.getPassword().length() < 6) {
+            throw new IllegalArgumentException("Invalid password");
+        }
         person.setPassword(encoder.encode(person.getPassword()));
         persons.save(person);
+    }
+
+    @ExceptionHandler(value = {NullPointerException.class})
+    public void handleNullPointerException(Exception e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setContentType("application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(new HashMap<>() {
+            {
+                put("message", "Some of fields empty");
+                put("details", e.getMessage());
+            }
+        }));
+        LOGGER.error(e.getMessage());
     }
 }
